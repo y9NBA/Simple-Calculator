3dc53f9f53578af901d8882e4ec459acf280d849
y9NBA <asarkrylov76212@gmail.com>
2024-06-13 15:07:30 +0300

    v3

--- src/Main.java
+++ src/Main.java
@@ -3,55 +3,64 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        String input = "";
-        String output = "";
+        String data = "";
 
         try (FileReader reader = new FileReader("input.txt")) {
             for (int c; (c = reader.read()) != -1; ) {
-                input += (((char)c));
+                data += ((char)c);
             }
         } catch (Exception ex) {
             System.out.println(ex.getMessage());
         }
 
-        try {
-            String[] line = input.split(" ");
+        String[] input = data.split("\n");
+        ArrayList<String> output = new ArrayList<>();
 
-            double number1 = Double.parseDouble(line[0]);
-            double number2 = Double.parseDouble(line[2]);
+        for (int i = 0; i < input.length; i++) {
+            String[] line = input[i].split(" ");
+            String outline = input[i].substring(0, input[i].length()-1) + " = ";
 
-            String operation = line[1];
+            try {
+                double number1 = Double.parseDouble(line[0]);
+                double number2 = Double.parseDouble(line[2]);
 
-            if (!"-*/+".contains(operation)) {
-                throw new Exception("Operation Error!");
+                String operation = line[1];
+
+                if (!"-*/+".contains(operation)) {
+                    throw new Exception("Operation Error!");
+                }
+
+                if (number2 == 0) {
+                    throw new Exception("Error! Division by zero");
+                }
+
+                switch (operation) {
+                    case "+":
+                        outline += number1 + number2;
+                        break;
+                    case "-":
+                        outline += number1 - number2;
+                        break;
+                    case "*":
+                        outline += number1 * number2;
+                        break;
+                    case "/":
+                        outline += number1 / number2;
+                        break;
+                }
+            } catch (NumberFormatException ex) {
+                outline += "Error! Not number";
+            } catch (Exception ex) {
+                outline += ex.getMessage();
             }
 
-            if (number2 == 0) {
-                throw new Exception("Error! Division by zero");
-            }
-
-            switch (operation) {
-                case "+":
-                    output += number1 + number2;
-                    break;
-                case "-":
-                    output += number1 - number2;
-                    break;
-                case "*":
-                    output += number1 * number2;
-                    break;
-                case "/":
-                    output += number1 / number2;
-                    break;
-            }
-        } catch (NumberFormatException ex) {
-            System.out.println("Error! Not number");
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
+            output.add(outline + "\n");
         }
 
         try (FileWriter writer = new FileWriter("output.txt", false)) {
-            writer.write(output);
+            for (String item: output) {
+                writer.write(item);
+            }
 
             writer.flush();
         } catch (Exception ex) {
